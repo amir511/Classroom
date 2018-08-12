d59e48c93b2893e641e8980926af597e016f668c
amir <amir.anwar.said@gmail.com>
2018-08-12 14:25:33 +0200

    feat: Add Student profile page and Student profile index page

--- students/models.py
+++ students/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
+
+class StudentsProfilesIndexPage(Page):
+    class_name = models.CharField(max_length=255)
+    description = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('class_name', classname="full"),
+        FieldPanel('description', classname="full"),
+    ]
+
+
+class StudentProfilePage(Page):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birthday = models.DateField()
+    mobile_number = models.CharField(max_length=255)
+    email = models.EmailField()
+    comments = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('first_name'),
+        FieldPanel('last_name'),
+        FieldPanel('birthday'),
+        FieldPanel('mobile_number'),
+        FieldPanel('email'),
+        FieldPanel('comments'),
+    ]
+
+
